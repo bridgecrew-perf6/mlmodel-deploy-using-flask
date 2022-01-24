682cade4adc44baad14e283db3e273074fccdfcd
debu-sinha <debusinha2009@gmail.com>
2022-01-24 12:27:06 -0500

    cleaned requirements.txt and documentation.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request
 import numpy as np
 import pandas
 import joblib
+
+#load  the model from the specified location.
 model_folder = 'models/'
 filePath = model_folder + 'best_model.pkl'
 file = open(filePath, "rb")
@@ -12,17 +14,25 @@ loaded_model = joblib.load(file)
 #create an instance of Flask
 app = Flask(__name__)
 
+
+#By default this home.html will be loaded from the templates folder when we hit the http://127.0.0.1:5000
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# prediction function
+# prediction function that makes use of the trained ML model to make predictions on the new data points provided.
 def ValuePredictor(to_predict_list):
+    """
+    take the new input parameters as list and return prediction made from the loaded model.
+    :param to_predict_list: list of new input parameters from the end user.
+    :return: salary prediction made by the model.
+    """
     to_predict = np.array(to_predict_list)
     test_df = pandas.DataFrame(to_predict, columns = ["age","workclass","education","educational-num","marital-status","occupation","relationship","race","gender","capital-gain","capital-loss","hours-per-week","native-country"])
     result = loaded_model.predict(test_df)
     return result[0]
- 
+
+# This route will return the result of the prediction made.
 @app.route('/result', methods = ['POST'])
 def result():
     if request.method == 'POST':
